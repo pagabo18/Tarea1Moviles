60423d754c99f87aa292c9c9ec7f0ba666044998
pagabo18 <pagabo18hotmail.com>
2022-01-30 22:56:50 -0600

    "subida a main"

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,159 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Material App',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class HomePage extends StatefulWidget {
+  HomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int _dec = 0;
-  int _inc = 0;
-  int _mail = 0;
-  int _phone = 0;
-  int _dir = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      _inc++;
-    });
-  }
-
-  void _dectrementCounter() {
-    setState(() {
-      _counter--;
-      _dec++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  int likes = 0;
+  int up = 0;
+  int down = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('App Iteso'),
-            ),
-            body: Column(
+    //Scaffold indica que se formara una pantalla
+    return Scaffold(
+      // Columna para alinear elementos en vertical (de arriba hacia abajo)
+      body: Column(
+        children: [
+          // Para agregar imagenes de una url
+          Image.network(
+            "https://cruce.iteso.mx/wp-content/uploads/sites/123/2018/04/Portada-2-e1525031912445.jpg",
+          ),
+          // Para poner elementos en renglon
+
+          Center(
+            heightFactor: 3,
+            child: Row(
+              // el eje main de la Row es horizontal (izq a dch)
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.network(
-                    "https://cruce.iteso.mx/wp-content/uploads/sites/123/2018/04/Portada-2-e1525031912445.jpg"),
-                ListTile(
-                  title: const Text(
-                      'EL ITESO, Universidad jesuita de guadalajara'),
-                  subtitle: const Text('Tlaquepaque'),
-                  trailing: Wrap(
-                    children: <Widget>[
-                      IconButton(
-                        icon: const Icon(Icons.thumb_up),
-                        onPressed: _incrementCounter,
-                        color: (_inc % 2 == 1) ? Colors.blue : Colors.grey,
-                      ),
-                      IconButton(
-                          icon: const Icon(Icons.thumb_down),
-                          onPressed: _dectrementCounter,
-                          color: (_dec % 2 == 1) ? Colors.red : Colors.grey),
-                      Text('$_counter'),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.mail),
-                          onPressed: () {
-                            setState(() {
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(SnackBar(
-                                content: const Text("Mail"),
-                              ));
-                              _mail++;
-                              print(_mail);
-                            });
-                          },
-                          color: (_mail % 2 == 1) ? Colors.blue : Colors.black,
-                        ),
-                        const Text("correo"),
-                      ],
+                    Text(
+                      "ITESO, Universidad Jesuita de Guadalajara",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                      ),
                     ),
-                    Column(
-                      children: [
-                        Center(
-                          child: IconButton(
-                            icon: const Icon(Icons.phone),
-                            onPressed: () {
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(const SnackBar(
-                                content: const Text("Telefono"),
-                                duration: const Duration(milliseconds: 3000),
-                              ));
-
-                              setState(() {
-                                _phone++;
-                              });
-                            },
-                            color:
-                                (_phone % 2 == 1) ? Colors.blue : Colors.black,
-                          ),
-                        ),
-                        const Text("telefono")
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.directions),
-                          onPressed: () {
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(const SnackBar(
-                              content: const Text("Direccion"),
-                              duration: const Duration(milliseconds: 3000),
-                            ));
-
-                            setState(() {
-                              _dir++;
-                            });
-                          },
-                          color: (_dir % 2 == 1) ? Colors.blue : Colors.black,
-                        ),
-                        const Text("correo"),
-                      ],
-                    ),
+                    Text("Tlaquepaque in the Guadalajara Metropolitan Area"),
                   ],
-                )
+                ),
+                IconButton(
+                  onPressed: () {
+                    likes++;
+                    if (up == 0) {
+                      up++;
+                    } else {
+                      up--;
+                    }
+                    // set state refresca pantalla para actualizar contenido
+                    setState(() {});
+                  },
+                  color: (up == 0) ? Colors.black : Colors.blue,
+                  icon: Icon(Icons.thumb_up),
+                ),
+                Text("$likes"),
+                IconButton(
+                  onPressed: () {
+                    (likes > 0) ? likes-- : likes;
+                    if (down == 0) {
+                      down++;
+                    } else {
+                      down--;
+                    }
+                    // set state refresca pantalla para actualizar contenido
+                    setState(() {});
+                  },
+                  color: (down == 0) ? Colors.black : Colors.red,
+                  icon: Icon(Icons.thumb_down),
+                ),
               ],
-            )));
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    iconSize: 64,
+                    onPressed: () {
+                      // Muestra un snack bar con texto
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Correo del iteso: correo@iteso.com"),
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.email),
+                  ),
+                  Text("Email"),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    iconSize: 64,
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("telefono del iteso: 3312415613"),
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.phone_forwarded),
+                  ),
+                  Text("Telefono"),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    iconSize: 64,
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Ruta del iteso"),
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.directions),
+                  ),
+                  Text("Ruta"),
+                ],
+              ),
+            ],
+          ),
+          // Para poner padding alrededor de un widget
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "The university has approximately 10,000 students. Its academic options include Civil Engineering and Architecture, Food Engineering, Education, Electronic Engineering, International Business, International Relations, Chemical Engineering, Philosophy, Psychology and Social Studies, and Networks and Telecommunications Engineering.[2] The university is affiliated to the Jesuit University System, which includes the Iberoamerican Universities in Acapulco, Mexico City, Jaltepec, León, Torreón, Puebla and Tijuana.[3] According to the vision of Jesuits, local businesspeople, and others who planned the university, it would combine professional training with a firm sense of social responsibility.",
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
